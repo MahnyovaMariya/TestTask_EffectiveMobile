2b984d3950316f914d26a6d9d066f1b97aa95563
MahnyovaMariya <mahnyovamariya@gmail.com>
2022-12-05 10:05:12 +0300

    Initial Commit

---
+++ TestTask_EffectiveMobile/TestTask_EffectiveMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTask_EffectiveMobileApp.swift
+//  TestTask_EffectiveMobile
+//
+//  Created by Мария on 05.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestTask_EffectiveMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
